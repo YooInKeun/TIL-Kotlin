a48ff592e713830a475d2f01e85d308df2a17974
유인근 <keun0390@naver.com>
2021-06-20 23:11:15 +0900

    chore: 기본 변수 사용법

---
+++ variable.kts
@@ -0,0 +1,9 @@
+// var는 값을 변경할 수 있다
+var count: Int = 5
+count = 10
+
+// val은 값을 변경할 수 없다
+val name: String = "yooinkeun"
+
+// 변수는 기본적으로 null을 할당할 수 없다. ?를 접미사로 지정하여 nullable하게 사용할 수 있다
+val nullable: String? = null
