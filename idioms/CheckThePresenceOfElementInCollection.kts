df1c2b55dbee151ec9af1d75a083bfe62185f4bc
유인근 <keun0390@naver.com>
2021-07-03 23:07:32 +0900

    chore: in 사용법

---
+++ idioms/CheckThePresenceOfElementInCollection.kts
@@ -0,0 +1,9 @@
+val students = listOf("Kevin", "Aiden", "Bob")
+
+if ("Aiden" in students) {
+    println("Adien is included")
+}
+
+if ("Aiden" !in students) {
+    println("Adien is not included")
+}
