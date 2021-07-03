bef251401b08bb44e153c761e045a39dfd47b1d4
유인근 <keun0390@naver.com>
2021-07-03 23:08:17 +0900

    chore: map 사용법

---
+++ idioms/Map.kts
@@ -0,0 +1,8 @@
+val unmodifiableMap = mapOf(0 to "a", 1 to "b", 2 to "c")
+println(unmodifiableMap[0])
+println(unmodifiableMap)
+//map[2] = "d" // read-only이기 때문에 에러 발생
+
+val mutableMap = mutableMapOf(0 to "a", 1 to "b", 2 to "c")
+mutableMap[2] = "d"
+println(mutableMap)
